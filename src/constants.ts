2cff0ae536dc897b0676320aac5ccb2459e0210f
Zecento <157219382+mellowtel@users.noreply.github.com>
2024-12-01 01:07:14 -0800

    bump to 700.0.4

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const VERSION: string = "700.0.3";
+export const VERSION: string = "700.0.4";
 export let MAX_DAILY_RATE: number = 500;
 export const REFRESH_INTERVAL: number = 1000 * 60 * 60 * 24; // 24 hours
