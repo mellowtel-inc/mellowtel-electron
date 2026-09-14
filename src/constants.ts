76f612ad7c1688ed8f90381b8db2da4f25462046
mellowtel-moeez <moeezahmad024@gmail.com>
2026-09-14 15:21:44 +0500

    chore: update version to 700.0.34 in package.json and constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const VERSION: string = "700.0.33";
+export const VERSION: string = "700.0.34";
 export let MAX_DAILY_RATE: number = 15000;
 export const REFRESH_INTERVAL: number = 1000 * 60 * 60 * 24; // 24 hours
 
