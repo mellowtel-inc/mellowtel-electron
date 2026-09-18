eb51a02af89de58a1f39403ff99bdf27e1d4f223
mellowtel-moeez <moeezahmad024@gmail.com>
2026-09-18 21:55:27 +0500

    700.0.36

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const VERSION: string = "700.0.35";
+export const VERSION: string = "700.0.36";
 export let MAX_DAILY_RATE: number = 15000;
 export const REFRESH_INTERVAL: number = 1000 * 60 * 60 * 24; // 24 hours
 
